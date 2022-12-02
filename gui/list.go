72e8796aca1d97c54491d55149b4f82aaf42d430
Duc Nguyen <nguyen@emersion.fr>
2022-12-02 17:59:03 +0100

    basic gui and better logs

---
+++ gui/list.go
@@ -0,0 +1,63 @@
+package gui
+
+import (
+	"log"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+var radios = []list.Item{
+	item{title: "FIP", desc: "I have ’em all over my house"},
+	item{title: "FIP Jazz", desc: "It's good on toast"},
+}
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
+
+func Render() {
+	m := model{list: list.New(radios, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "FIP Radios"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,28 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"io"
 	"log"
+	"os"
 
 	"github.com/DucNg/fip-player/dbus"
+	"github.com/DucNg/fip-player/gui"
 	"github.com/DucNg/fip-player/player"
 )
 
 func main() {
-	log.SetFlags(log.Lshortfile) // Enable line number on error
+	enableDebugLogs()
 
 	mpv := &player.MPV{}
-	mpvChan, _ := mpv.Initialize()
+	mpv.Initialize()
 	mpv.SendCommand([]string{"loadfile", "https://stream.radiofrance.fr/fip/fip.m3u8?id=radiofrance"})
 
 	go dbus.RunDbusListener(mpv)
 
-	for event := range mpvChan {
-		switch event {
-		case player.STATE_PLAYING:
-			fmt.Println("Playing...")
-		case player.STATE_PAUSED:
-			fmt.Println("Pausing...")
-		}
+	gui.Render()
+
+	// for event := range mpvChan {
+	// 	switch event {
+	// 	case player.STATE_PLAYING:
+	// 		fmt.Println("Playing...")
+	// 	case player.STATE_PAUSED:
+	// 		fmt.Println("Pausing...")
+	// 	}
+	// }
+}
+
+func enableDebugLogs() {
+	debug := flag.Bool("d", false, "enables debug output to /tmp")
+	flag.Parse()
+
+	if !*debug {
+		log.SetOutput(io.Discard)
+		return
 	}
+
+	log.SetFlags(log.Lshortfile) // Enable line number on error
+
+	logFile, err := os.CreateTemp("", "fip-player-log")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.SetOutput(logFile)
 }
